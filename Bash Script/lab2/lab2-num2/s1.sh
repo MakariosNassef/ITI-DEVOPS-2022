266f563da2cc188349c5cf1429cac830352761d4
MakariosNassef <makarios059@gmail.com>
2022-11-27 17:59:00 +0200

    Add files via upload

---
+++ Bash Script/lab2/lab2-num2/s1.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#first way to call s2 using export
+#export x=5
+
+--------------------------------------------------------------
+#2nd way to call s2 using sorcing
+x=5
+. s2.sh
+
---
+++ Bash Script/lab2/lab2-num2/s2.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+echo "val of x = $x"
+echo "hi from s2.sh"
